48e7082ed95560f97b8482cb7d2aeb8d4d6a361a
Frezcirno <2277861660@qq.com>
2020-11-30 23:12:32 +0800

    更新数据库模型, 添加题材表

--- dw_mysql8.sql
+++ dw_mysql8.sql
@@ -2,6 +2,7 @@ ALTER TABLE `Actin` DROP FOREIGN KEY `fk_Direct_Movie_1_copy_1`;
 ALTER TABLE `Actin` DROP FOREIGN KEY `fk_Direct_Person_1_copy_1`;
 ALTER TABLE `Direct` DROP FOREIGN KEY `fk_Direct_Movie_1`;
 ALTER TABLE `Direct` DROP FOREIGN KEY `fk_Direct_Person_1`;
+ALTER TABLE `Movie` DROP FOREIGN KEY `fk_Movie_Genres_1`;
 ALTER TABLE `Review` DROP FOREIGN KEY `fk_Review_Movie_1`;
 ALTER TABLE `Review` DROP FOREIGN KEY `fk_Review_User_1`;
 ALTER TABLE `Star` DROP FOREIGN KEY `fk_Direct_Movie_1_copy_2`;
@@ -10,6 +11,7 @@ ALTER TABLE `Star` DROP FOREIGN KEY `fk_Direct_Person_1_copy_2`;
 DROP TABLE `Act`;
 DROP TABLE `Actin`;
 DROP TABLE `Direct`;
+DROP TABLE `Genres`;
 DROP TABLE `Movie`;
 DROP TABLE `Person`;
 DROP TABLE `Review`;
@@ -31,11 +33,18 @@ CREATE TABLE `Direct`  (
   PRIMARY KEY (`asin`, `pid`)
 );
 
+CREATE TABLE `Genres`  (
+  `id` int NOT NULL,
+  `name` varchar(255) NULL COMMENT '题材名',
+  PRIMARY KEY (`id`)
+);
+
 CREATE TABLE `Movie`  (
   `asin` varchar(255) NOT NULL COMMENT 'ASIN码',
   `title` varchar(255) NULL COMMENT '电影名',
   `score` float NULL COMMENT '打分',
   `release_date` datetime NULL COMMENT '上映时间',
+  `genre_id` int NULL COMMENT '题材id',
   PRIMARY KEY (`asin`)
 );
 
@@ -74,6 +83,7 @@ ALTER TABLE `Actin` ADD CONSTRAINT `fk_Direct_Movie_1_copy_1` FOREIGN KEY (`asin
 ALTER TABLE `Actin` ADD CONSTRAINT `fk_Direct_Person_1_copy_1` FOREIGN KEY (`pid`) REFERENCES `Person` (`id`);
 ALTER TABLE `Direct` ADD CONSTRAINT `fk_Direct_Movie_1` FOREIGN KEY (`asin`) REFERENCES `Movie` (`asin`);
 ALTER TABLE `Direct` ADD CONSTRAINT `fk_Direct_Person_1` FOREIGN KEY (`pid`) REFERENCES `Person` (`id`);
+ALTER TABLE `Movie` ADD CONSTRAINT `fk_Movie_Genres_1` FOREIGN KEY (`genre_id`) REFERENCES `Genres` (`id`);
 ALTER TABLE `Review` ADD CONSTRAINT `fk_Review_Movie_1` FOREIGN KEY (`asin`) REFERENCES `Movie` (`asin`);
 ALTER TABLE `Review` ADD CONSTRAINT `fk_Review_User_1` FOREIGN KEY (`id`) REFERENCES `User` (`id`);
 ALTER TABLE `Star` ADD CONSTRAINT `fk_Direct_Movie_1_copy_2` FOREIGN KEY (`asin`) REFERENCES `Movie` (`asin`);
