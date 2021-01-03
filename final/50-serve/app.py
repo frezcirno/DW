479dd85451d377d7f965bd968be5f184f678e96e
Frezcirno <2277861660@qq.com>
2021-01-03 21:26:29 +0800

    genre -> genres

--- final/50-serve/app.py
+++ final/50-serve/app.py
@@ -1,4 +1,5 @@
 from time import perf_counter
+from typing import List
 import ujson as json
 import mysql.connector
 import pyhive
@@ -52,7 +53,7 @@ def neo4j_query(cypher, param=None):
 season2months = {"1": [1, 2, 3], "2": [4, 5, 6], "3": [7, 8, 9], "4": [10, 11, 12]}
 
 
-@app.route('/api/combine')
+@app.route('/api/combine/product')
 def combine_product():
     mysql = mysql_product()
     neo4j = neo4j_product()
@@ -73,7 +74,7 @@ def mysql_any():
     return mysql_query(sql)
 
 
-@app.route("/api/mysql")
+@app.route("/api/mysql/product")
 def mysql_product():
     """
     按照时间进行查询及统计（例如XX年有多少电影，XX年XX月有多少电影，XX年XX季度有多少电影，周二新增多少电影等）
@@ -154,12 +155,17 @@ def mysql_product():
         where.append("support_actor = %s")
         param.append(support_actor)
 
-    genre = request.args.get("genre", 0)
-    if genre:
+    genres = request.args.getlist("genre[]")
+    if genres:
         _from.add("product")
         _from.add("product_genres")
-        where.append("genre = %s")
-        param.append(genre)
+        where.append(' and '.join(["genre = %s" for _ in genres]))
+        param += genres
+
+    # genre = request.args.get("genre", 0)
+    # if genre:
+    #     where.append("genre = %s")
+    #     param.append(genre)
 
     offset = request.args.get("offset", 0)
 
@@ -179,18 +185,144 @@ def mysql_product():
     return {"count": count[0]["num"], 'time': time, "data": res}
 
 
-@app.route("/api/neo4j/close")
-def neo4j_close():
+@app.route('/api/mysql/movie')
+def mysql_movie():
+    """
+    按照时间进行查询及统计（例如XX年有多少电影，XX年XX月有多少电影，XX年XX季度有多少电影，周二新增多少电影等）
+    按照电影名称进行查询及统计（例如 XX电影共有多少版本等）
+    按照用户评价进行查询及统计（例如用户评分3分以上的电影有哪些，用户评价中有正面评价的电影有哪些等）
+    按照导演进行查询及统计（例如 XX导演共有多少电影等）
+    按照演员进行查询及统计（例如 XX演员主演多少电影，XX演员参演多少电影等）
+    按照电影类别进行查询及统计（例如 Action电影共有多少，Adventure电影共有多少等）
+    """
+    _from = set()
+    where = []
+    param = []
+
+    y = request.args.get("y", 0)
+    if y:
+        _from.add("product")
+        where.append("y=%s")
+        param.append(y)
+
+    m = request.args.get("m", 0)
+    if m:
+        _from.add("product")
+        where.append("m=%s")
+        param.append(m)
+
+    d = request.args.get("d", 0)
+    if d:
+        _from.add("product")
+        where.append("d=%s")
+        param.append(d)
+
+    season = request.args.get("season", 0)
+    if season:
+        _from.add("product")
+        where.append("(m=%s or m=%s or m=%s)")
+        param += season2months[season]
+
+    weekday = request.args.get("weekday", 0)
+    if weekday:
+        _from.add("product")
+        where.append("weekday=%s")
+        param.append(weekday)
+
+    asin = request.args.get("asin", 0)
+    if asin:
+        _from.add("product")
+        where.append("asin=%s")
+        param.append(asin)
+
+    title = request.args.get("title", 0)
+    if title:
+        _from.add("product")
+        where.append(f"(title like '%{title}%')")
+
+    rating = request.args.get("rating", 0)
+    if rating:
+        _from.add("product")
+        where.append(f"rating >= {rating}")
+
+    director = request.args.get("director", 0)
+    if director:
+        _from.add("product")
+        _from.add("product_director")
+        where.append("director = %s")
+        param.append(director)
+
+    actor = request.args.get("actor", 0)
+    if actor:
+        _from.add("product")
+        _from.add("product_actor")
+        where.append("actor = %s")
+        param.append(actor)
+
+    support_actor = request.args.get("support_actor", 0)
+    if support_actor:
+        _from.add("product")
+        _from.add("product_support_actor")
+        where.append("support_actor = %s")
+        param.append(support_actor)
+
+    genres = request.args.getlist("genre[]")
+    if genres:
+        _from.add("product")
+        _from.add("product_genres")
+        where.append(' and '.join(["genre = %s" for _ in genres]))
+        param += genres
+
+    # genre = request.args.getlist("genre", 0)
+    # if genre:
+    #     where.append("genre = %s")
+    #     param.append(genre)
+
+    offset = request.args.get("offset", 0)
+
+    sql = "select product.movie" + " from " + " natural join ".join(_from) + \
+        " where " + " and ".join(where) + f" limit {offset},100"
+
+    sql = f"select * from product where movie=({sql})"
+
+    try:
+        start = perf_counter()
+        res = mysql_query(sql, param)
+        time = 1000 * (perf_counter() - start)
+    except mysql.connector.errors.DataError:
+        res = []
+        time = 0
+
+    return {"count": len(res), 'time': time, "data": res}
+
+
+@app.route("/api/neo4j/relation")
+def neo4j_relation():
     """
     按照演员和导演的关系进行查询及统计（例如经常合作的演员有哪些，经常合作的导演和演员有哪些）
     """
-    count = request.args.get("count", 100)
+    director = request.args.get("director", 0)
+    actor = request.args.get("actor", 0)
+    support_actor = request.args.get("support_actor", 0)
+    skip = request.args.get("skip", 0)
 
-    query_result = neo4j_query(
-        f"MATCH path = (a:Actor)--(p:Product)--(d:Director) WITH a, d, COUNT(*) AS num ORDER BY num DESC RETURN a,d,num limit {count}"
-    )
+    if director and actor:
+        cypher = f'match (a:Actor{{ actor:"{actor}" }})--(p:Product)--(d:Director{{ director:"{director}" }}) RETURN p'
+    elif director and support_actor:
+        cypher = f'match (s:SupportActor{{ support_actor:"{support_actor}" }})--(p:Product)--(d:Director{{ director:"{director}" }}) RETURN p'
+    elif actor and support_actor:
+        cypher = f'match (a:Actor{{ actor:"{actor}" }})--(p:Product)--(s:SupportActor{{ support_actor:"{support_actor}" }}) RETURN p'
+    else:
+        return {"count": 0, 'time': 0, "data": []}
 
-    return {"count": len(query_result)}
+    print(cypher)
+
+    start = perf_counter()
+    res = neo4j_query(cypher)
+    time = 1000 * (perf_counter() - start)
+
+    res = [dict(zip(row[0].keys(), row[0].values())) for row in res]
+    return {"count": len(res), 'time': time, "data": res}
 
 
 @app.route("/api/neo4j/sql")
@@ -199,7 +331,7 @@ def neo4j_any():
     return neo4j_query(cypher)
 
 
-@app.route("/api/neo4j")
+@app.route("/api/neo4j/product")
 def neo4j_product():
     where = []
 
@@ -248,9 +380,13 @@ def neo4j_product():
     if support_actor:
         where.append(f"s.support_actor = '{support_actor}'")
 
-    genre = request.args.get("genre", 0)
-    if genre:
-        where.append(f"g.genre = '{genre}'")
+    genres = request.args.getlist("genre[]")
+    if genres:
+        stat = ' and '.join([f"g.genre = '{g}'" for g in genres])
+        where.append(f"({stat})")
+
+    # elif genre:
+    #     where.append(f"g.genre = '{genre}'")
 
     skip = request.args.get("skip", 0)
 
@@ -270,7 +406,7 @@ def neo4j_product():
     return {"count": res_count[0][0], 'time': time, "data": res}
 
 
-@app.route('/api/hive')
+@app.route('/api/hive/product')
 def hive_product():
     return {'count': 0, 'data': [], 'time': 0}
 
